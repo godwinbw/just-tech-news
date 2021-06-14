6e55a7241ebfd000f7aeea6770b6ca04b69168b2
Bryan Godwin <godwin.bryan.w@gmail.com>
2021-06-14 17:46:25 -0500

    updated code to match lesson download

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,21 +1,25 @@
 async function editFormHandler(event) {
   event.preventDefault();
 
-  const id = window.location.toString().split("/")[
-    window.location.toString().split("/").length - 1
+  const title = document.querySelector('input[name="post-title"]').value.trim();
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
   ];
-
-  await fetch(`/api/posts/${id}`, {
-    method: "PUT",
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
     body: JSON.stringify({
-      title,
+      title
     }),
     headers: {
-      "Content-Type": "application/json",
-    },
+      'Content-Type': 'application/json'
+    }
   });
+
+  if (response.ok) {
+    document.location.replace('/dashboard/');
+  } else {
+    alert(response.statusText);
+  }
 }
 
-document
-  .querySelector(".edit-post-form")
-  .addEventListener("submit", editFormHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
